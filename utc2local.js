79152cc41e47a08414c033755a503002ad564638
Jerry Wu <mis.jerrywu@gmail.com>
2013-01-28 20:01:41 -0800

    Create utc2local.js

---
+++ utc2local.js
@@ -0,0 +1,90 @@
+(function($){
+  $.fn.utc2local = function() {
+	
+		$(this).each(function(){
+			
+			var utc_datetime = $.trim($(this).html());
+		
+			utc_datetime = utc_datetime.split(' ');
+			utc_date = utc_datetime[0];
+			utc_date = utc_date.split('-');
+			utc_time = utc_datetime[1];
+			utc_time = utc_time.split(':');
+			utc_datetime = new Date(utc_date[0], parseInt(utc_date[1]) - 1, utc_date[2], utc_time[0], utc_time[1], utc_time[2], 0);
+			var local_datetime = utc_datetime;
+			local_datetime.setMinutes(utc_datetime.getMinutes() - utc_datetime.getTimezoneOffset());
+			
+			var week_day  = '';
+			
+			switch(local_datetime.getDay())
+			{
+				case 0:
+					week_day = 'Sun';
+					break;
+				case 1:
+					week_day = 'Mon';
+					break;
+				case 2:
+					week_day = 'Tue';
+					break;
+				case 3:
+					week_day = 'Wed';
+					break;
+				case 4:
+					week_day = 'Thu';
+					break;
+				case 5:
+					week_day = 'Fri';
+					break;
+				case 6:
+					week_day = 'Sat';
+					break;
+			}
+			
+			var month  = '';
+			
+			switch(local_datetime.getMonth())
+			{
+				case 0:
+					month = 'Jan';
+					break;
+				case 1:
+					month = 'Feb';
+					break;
+				case 2:
+					month = 'Mar';
+					break;
+				case 3:
+					month = 'Apr';
+					break;
+				case 4:
+					month = 'May';
+					break;
+				case 5:
+					month = 'Jun';
+					break;
+				case 6:
+					day = 'Jul';
+					break;
+				case 7:
+					day = 'Aug';
+					break;
+				case 8:
+					day = 'Sep';
+					break;
+				case 9:
+					day = 'Oct';
+					break;
+				case 10:
+					day = 'Nov';
+					break;
+				case 11:
+					day = 'Dec';
+					break;
+			}
+			
+			$(this).html(week_day + ', '+ month + ' ' + local_datetime.getDate() + ', ' + local_datetime.getFullYear() + ' ' + local_datetime.getHours() + ':' + local_datetime.getMinutes());
+		});	
+	};
+	
+})(jQuery);
